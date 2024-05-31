e900ff8982d2ea57efc2bcfae2e6ff2cc3d0187d
Hasun Nilupul <hasunnilupul16@gmail.com>
2024-05-31 20:58:31 +0530

    installed nuxt/ui

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["nuxt-vuefire"],
+  modules: ["nuxt-vuefire", "@nuxt/ui"],
   vuefire: {
     config: {
       apiKey: process.env.FIREBASE_API_KEY,
